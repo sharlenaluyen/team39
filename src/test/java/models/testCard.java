485fa50238c64f06acffa4a285ce1d4b1f094190
Cody Schlabach <schlabac@oregonstate.edu>
2017-12-03 17:54:53 -0800

    added test cases

--- src/test/java/models/testCard.java
+++ src/test/java/models/testCard.java
@@ -8,11 +8,7 @@ import static org.junit.Assert.*;
  * Created by michaelhilton on 1/26/16.
  */
 public class testCard {
-    @Test
-    public void testGetSuit(){
-        Card c = new Card(5,Suit.Clubs);
-        assertEquals(Suit.Clubs,c.getSuit());
-    }
+   
 
     @Test
     public void testToString(){
@@ -23,13 +19,12 @@ public class testCard {
     @Test
     public void testMoveCard(){
         Game g = new Game();
-        g.buildDeck();
         g.customDeal(0,3,6,9);
         g.remove(2);
-        assertEquals(0,g.cols.get(2).size());
+        assertEquals(0,g.columns.get(2).cards.size());
         g.move(0,2);
-        assertEquals(1,g.cols.get(2).size());
-        assertEquals(0,g.cols.get(0).size());
+        assertEquals(1,g.columns.get(2).cards.size());
+        assertEquals(0,g.columns.get(0).cards.size());
     }
 
 
