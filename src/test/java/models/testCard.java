e7f456c19838a8251e4ffecef60e43f6034ba247
Sharlena <9398483+sharlenaluyen@users.noreply.github.com>
2017-12-03 17:52:53 -0800

    adding test for card value

--- src/test/java/models/testCard.java
+++ src/test/java/models/testCard.java
@@ -8,6 +8,13 @@ import static org.junit.Assert.*;
  * Created by michaelhilton on 1/26/16.
  */
 public class testCard {
+
+    @Test
+    public void testGetValue(){
+        Card c = new Card(7,Suit.Clubs);
+        assertEquals(7,c.getValue());
+    }
+    
     @Test
     public void testGetSuit(){
         Card c = new Card(5,Suit.Clubs);
